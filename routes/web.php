ef761590833f07e12a5f835ad2cc7391c16874b0
Anne Hazel <anne.hazel2@gmail.com>
2017-05-03 21:36:15 -0400

    added debugbar and logs viewer for local environment

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,16 @@
 */
 
 Route::get('/', 'BiographyController@index');
+
+
+
+
+
+
+
+/*
+ * Routes for error handling and testing
+ */
+if(config('app.env') == 'local'){
+  Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+} 
